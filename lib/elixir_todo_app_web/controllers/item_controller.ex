0159651fc2a1b2c790157e3596d9ad602fe35f18
Ahmed Ismail <iamahmedismail19@gmail.com>
2023-06-20 16:28:22 +0500

    Minor enhancements in the list/item controllers

--- lib/elixir_todo_app_web/controllers/item_controller.ex
+++ lib/elixir_todo_app_web/controllers/item_controller.ex
@@ -4,27 +4,37 @@ defmodule ElixirTodoAppWeb.ItemController do
 
   def index(conn, %{"list_id" => list_id}) do
     items = Items.list_all(list_id)
-    render(conn, "index.json", items: items)
+    conn
+    |> put_resp_content_type("application/json")
+    |> json(%{items: items})
   end
 
   def create(conn, %{"list_id" => list_id, "item" => item_params}) do
     list = Lists.get_list(list_id)
 
-    if list.archived do
-      conn
-      |> render("message.json", message: "List is archived, can't create an item in it")
-    else
-      case Items.create_item(list_id, item_params) do
-        {:ok, item} ->
-          conn
-          |> put_status(:created)
-          |> render("show.json", item: item)
+    cond do
+      is_nil(list) ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> json(%{message: "No such list exists"})
 
-        {:error, changeset} ->
-          conn
-          |> put_status(:unprocessable_entity)
-          |> render("error.json", changeset: changeset)
-      end
+      list.archived ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> json(%{message: "List is archived, can't create an item in it"})
+
+      true ->
+        case Items.create_item(list_id, item_params) do
+          {:ok, item} ->
+            conn
+            |> put_status(:created)
+            |> json(%{item: item})
+
+          {:error, changeset} ->
+            conn
+            |> put_resp_content_type("application/json")
+            |> json(%{message: "Unidentified error"})
+        end
     end
   end
 
@@ -32,21 +42,34 @@ defmodule ElixirTodoAppWeb.ItemController do
     item = Items.get_item(id)
     list = Lists.get_list(list_id)
 
-    if list.archived do
-      conn
-      |> render("message.json", message: "List is archived, can't update any item")
-    else
-      case Items.update_item(item, item_params) do
-        {:ok, updated_item} ->
-          conn
-          |> put_status(:ok)
-          |> render("show.json", item: updated_item)
+    cond do
+      is_nil(list) ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> json(%{message: "No such list exists"})
 
-        {:error, changeset} ->
-          conn
-          |> put_status(:unprocessable_entity)
-          |> render("error.json", changeset: changeset)
-      end
+      is_nil(item) ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> json(%{message: "No such item exists"})
+
+      list.archived ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> json(%{message: "List is archived, can't update any item"})
+
+      true ->
+        case Items.update_item(item, item_params) do
+          {:ok, updated_item} ->
+            conn
+            |> put_status(:ok)
+            |> json(%{item: updated_item})
+
+          {:error, changeset} ->
+            conn
+            |> put_resp_content_type("application/json")
+            |> json(%{message: "Unidentified error"})
+        end
     end
   end
 end
--- lib/elixir_todo_app_web/controllers/list_controller.ex
+++ lib/elixir_todo_app_web/controllers/list_controller.ex
@@ -1,44 +1,54 @@
 defmodule ElixirTodoAppWeb.ListController do
   use ElixirTodoAppWeb, :controller
-  alias ElixirTodoApp.Lists
+  alias ElixirTodoApp.{Lists, List}
 
   def index(conn, _params \\ %{}) do
     lists = Lists.list_all()
-    render(conn, "index.json", lists: lists)
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> json(%{lists: lists})
   end
 
   def create(conn, %{"list" => list_params}) do
     case Lists.create_list(list_params) do
       {:ok, list} ->
         conn
-        |> put_status(:created)
-        |> render("show.json", list: list)
+        |> put_resp_content_type("application/json")
+        |> json(%{list: list})
 
       {:error, changeset} ->
         conn
-        |> put_status(:unprocessable_entity)
-        |> render("error.json", changeset: changeset)
+        |> put_resp_content_type("application/json")
+        |> json(%{message: "Unidentified error"})
     end
   end
 
   def update(conn, %{"id" => id, "list" => list_params}) do
     list = Lists.get_list(id)
+    cond do
+      is_nil(list) ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> json(%{message: "No such list exists"})
 
-    if list.archived and list_params["title"] do
-      conn
-      |> render("message.json", message: "List is archived, can't update its title")
-    else
-      case Lists.update_list(list, list_params) do
-        {:ok, updated_list} ->
-          conn
-          |> put_status(:ok)
-          |> render("show.json", list: updated_list)
+      not is_nil(list) and list.archived and list_params["title"] ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> json(%{message: "List is archived, can't update its title"})
 
-        {:error, changeset} ->
-          conn
-          |> put_status(:unprocessable_entity)
-          |> render("error.json", changeset: changeset)
-      end
+      true ->
+        case Lists.update_list(list, list_params) do
+          {:ok, updated_list} ->
+            conn
+            |> put_resp_content_type("application/json")
+            |> json(%{list: updated_list})
+
+          {:error, changeset} ->
+            conn
+            |> put_resp_content_type("application/json")
+            |> json(%{message: "Unidentified error"})
+        end
     end
   end
 end
