767bc5f856b9c70815e80878e60754471978a59f
Ahmed Ismail <iamahmedismail19@gmail.com>
2023-06-20 16:29:24 +0500

    Add the http-client for hitting the APIs

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule ElixirTodoApp.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:timex, "~> 3.7"}
+      {:timex, "~> 3.7"},
+      {:tesla, "~> 1.4"}
     ]
   end
 
